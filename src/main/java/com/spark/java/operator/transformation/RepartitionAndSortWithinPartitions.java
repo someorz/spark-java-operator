0c441bafb12ebf58efb4c5f6576cc24563ee2ff6
machi <machi@aerozhonghuan.com>
2018-11-01 10:06:14 +0800

    修改

--- src/main/java/com/spark/java/operator/transformation/RepartitionAndSortWithinPartitions.java
+++ src/main/java/com/spark/java/operator/transformation/RepartitionAndSortWithinPartitions.java
@@ -2,15 +2,15 @@ package com.spark.java.operator.transformation;
 
 import org.apache.spark.Partitioner;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
+import scala.Serializable;
 import scala.Tuple2;
+import scala.Tuple3;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
 
 /**
  * @author machi
@@ -26,26 +26,81 @@ public class RepartitionAndSortWithinPartitions {
 
         JavaSparkContext jsc = new JavaSparkContext(session.sparkContext());
 
-        List<Integer> data = Arrays.asList(1, 2, 4, 3, 5, 6, 7);
-        JavaRDD<Integer> javaRDD = jsc.parallelize(data);
-        final Random random = new Random(); JavaPairRDD<Integer, Integer> javaPairRDD = javaRDD.mapToPair(
-                (PairFunction<Integer, Integer, Integer>) integer -> new Tuple2<>(integer, random.nextInt(10)));
+        List<Tuple2< PartKey,Tuple3<Integer,Integer,Integer>>> data = Arrays.asList(
+                Tuple2.apply(new PartKey(1L,1D),Tuple3.apply(1,1,1)),
+                Tuple2.apply(new PartKey(1L,2D),Tuple3.apply(2,1,2)),
+                Tuple2.apply(new PartKey(1L,3D),Tuple3.apply(3,1,3)),
+                Tuple2.apply(new PartKey(2L,5D),Tuple3.apply(4,2,4)),
+                Tuple2.apply(new PartKey(2L,4D),Tuple3.apply(5,2,5)));
+        JavaPairRDD<PartKey, Tuple3<Integer, Integer, Integer>> javaRDD = jsc.parallelizePairs(data);
 
-        JavaPairRDD<Integer, Integer> RepartitionAndSortWithPartitionsRDD = javaPairRDD.repartitionAndSortWithinPartitions(
-                new Partitioner() {
-                    @Override
-                    public int numPartitions() {
-                        return 2;
-                    }
+        JavaPairRDD<PartKey, Tuple3<Integer, Integer, Integer>> RepartitionAndSortWithPartitionsRDD = javaRDD.repartitionAndSortWithinPartitions(
+                new StudentPartitioner(10) , new Comp());
 
-                    @Override
-                    public int getPartition(Object key) {
-                        return key.toString().hashCode() % numPartitions();
-                    }
-                });
         System.out.println(RepartitionAndSortWithPartitionsRDD.collect());
 
         jsc.close();
         session.stop();
+    }
+
+
+    private static class Comp implements Comparator<PartKey>,Serializable{
+        @Override
+        public int compare(PartKey o1, PartKey o2) {
+            return o1.getAvg().compareTo(o2.getAvg());
+        }
+    }
+
+
+    static class PartKey implements Serializable {
+        private Long mid;
+        private Double avg;
+
+        public PartKey(Long mid, Double avg) {
+            this.mid = mid;
+            this.avg = avg;
+        }
+
+        public Long getMid() {
+            return mid;
+        }
+
+        public void setMid(Long mid) {
+            this.mid = mid;
+        }
+
+        public Double getAvg() {
+            return avg;
+        }
+
+        public void setAvg(Double avg) {
+            this.avg = avg;
+        }
+
+
+    }
+
+
+    static class StudentPartitioner extends Partitioner {
+
+        private int partitionsNum;
+
+        public StudentPartitioner(int partitionsNum) {
+            this.partitionsNum = partitionsNum;
+        }
+
+        @Override
+        public int numPartitions() {
+            return partitionsNum;
+        }
+
+        @Override
+        public int getPartition(Object key) {
+            PartKey key1 = (PartKey) key;
+            if (key1.getMid() == null) {
+                key1.setMid(0L);
+            }
+            return key1.getMid().hashCode() % partitionsNum;
+        }
     }
 }
--- src/main/java/com/spark/java/operator/transformation/ZipWithIndex.java
+++ src/main/java/com/spark/java/operator/transformation/ZipWithIndex.java
@@ -4,6 +4,7 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple3;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,11 +23,10 @@ public class ZipWithIndex {
 
         JavaSparkContext jsc = new JavaSparkContext(session.sparkContext());
 
-        List<Integer> data = Arrays.asList(5, 1, 1, 4, 4, 2, 2);
-        JavaRDD<Integer> javaRDD = jsc.parallelize(data, 3);
-        JavaPairRDD<Integer, Long> zipWithIndexRDD = javaRDD.zipWithIndex();
+        List<Tuple3<Integer,Integer,Integer>> data = Arrays.asList(Tuple3.apply(1,1,1), Tuple3.apply(2,1,2), Tuple3.apply(3,1,3), Tuple3.apply(4,2,4), Tuple3.apply(5,2,5));
+        JavaRDD<Tuple3<Integer,Integer,Integer>> javaRDD = jsc.parallelize(data, 3);
+        JavaPairRDD<Tuple3<Integer, Integer, Integer>, Long> zipWithIndexRDD = javaRDD.zipWithIndex();
         System.out.println(zipWithIndexRDD.collect());
-
         jsc.close();
         session.stop();
     }
